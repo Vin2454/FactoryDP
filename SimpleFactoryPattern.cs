c0af1edf68bb1324401eb94ec526e60cdd482293
Vin2454 <vin2454@gmail.com>
2020-07-02 10:43:20 -0700

    Update SimpleFactoryPattern.cs

--- SimpleFactoryPattern.cs
+++ SimpleFactoryPattern.cs
@@ -14,19 +14,12 @@ namespace DesignPattern
                 Console.WriteLine("Enter annual income...");
                 string annualIncomeString = Console.ReadLine();
                 Decimal annualIncome = Convert.ToDecimal(annualIncomeString);
-                Console.WriteLine("Enter card type...");
-                string cardType = Console.ReadLine();
-
                 CardFactory cardFactory = new CardFactory();
+                string cardType = "credit";
                 if (cardType.ToLower() == "credit")
                 {
                     PostPaidCard postPaidCard = cardFactory.GetPostPaidCard(annualIncome);
                     Console.WriteLine($"{typeof(PostPaidCard)} {postPaidCard.Name} : {annualIncome}");
-                }
-                else if (cardType.ToLower() == "debit")
-                {
-                    PrepaidCard prePaidCard = cardFactory.GetPrePaidCard(annualIncome);
-                    Console.WriteLine($"{typeof(PrepaidCard)} {prePaidCard.Name} : {annualIncome}");
                 }
                 else
                 {
@@ -58,28 +51,15 @@ namespace DesignPattern
             }
             return postPaidCard;
         }
-
-        internal PrepaidCard GetPrePaidCard(decimal amount)
-        {
-            PrepaidCard prePaidCard = new PrepaidCard();
-            if (amount > 5000)
-            {
-                prePaidCard = new Hdfc();
-                prePaidCard.Name = "Hdfc";
-            }
-            else
-            {
-                prePaidCard = new Icici();
-                prePaidCard.Name = "Icici";
-            }
-            return prePaidCard;
-        }
     }
-    class PostPaidCard
+    class Card
     {
         public string Name { get; set; }
-        public decimal AnnualFee { get; set; }
         public string Type { get; set; }
+    }
+    class PostPaidCard : Card
+    {
+        public decimal AnnualFee { get; set; }
         public string Balance { get; set; }
         public decimal Charge { get; set; }
     }
@@ -95,22 +75,5 @@ namespace DesignPattern
     class Smart : PostPaidCard
     {
 
-    }
-
-    //debit cards(prepaid)
-    class PrepaidCard
-    {
-        public string Name { get; set; }
-        public decimal AnnualFee { get; set; }
-        public string Type { get; set; }
-        public string Balance { get; set; }
-    }
-    class Icici : PrepaidCard
-    {
-
-    }
-    class Hdfc : PrepaidCard
-    {
-        public string Feedback { get; set; }
     }
 }
