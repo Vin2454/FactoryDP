3cfee540ab8ca0cdd43848978702e92c3e258f08
Vin2454 <vin2454@gmail.com>
2020-07-02 10:43:48 -0700

    Update FactoryMethod.cs

--- FactoryMethod.cs
+++ FactoryMethod.cs
@@ -14,18 +14,12 @@ namespace DesignPattern
                 Console.WriteLine("Enter annual income...");
                 string annualIncomeString = Console.ReadLine();
                 Decimal annualIncome = Convert.ToDecimal(annualIncomeString);
-                Console.WriteLine("Enter card type...");
-                string cardType = Console.ReadLine();
                 FactoryWithFactoryMethod factoryWithFactoryMethod = new CardFactory();
+                string cardType = "credit";
                 if (cardType.ToLower() == "credit")
                 {
                     PostPaidCard postPaidCard = factoryWithFactoryMethod.GetPostPaidCard(annualIncome);
                     Console.WriteLine($"{typeof(PostPaidCard)} {postPaidCard.Name} : {annualIncome}");
-                }
-                else if (cardType.ToLower() == "debit")
-                {
-                    PrepaidCard prePaidCard = factoryWithFactoryMethod.GetPrePaidCard(annualIncome);
-                    Console.WriteLine($"{typeof(PrepaidCard)} {prePaidCard.Name} : {annualIncome}");
                 }
                 else
                 {
@@ -37,7 +31,6 @@ namespace DesignPattern
     abstract class FactoryWithFactoryMethod
     {
         internal abstract PostPaidCard GetPostPaidCard(decimal amount);
-        internal abstract PrepaidCard GetPrePaidCard(decimal amount);
     }
     class CardFactory : FactoryWithFactoryMethod
     {
@@ -61,28 +54,15 @@ namespace DesignPattern
             }
             return postPaidCard;
         }
-
-        internal override PrepaidCard GetPrePaidCard(decimal amount)
-        {
-            PrepaidCard prePaidCard = new PrepaidCard();
-            if (amount > 5000)
-            {
-                prePaidCard = new Hdfc();
-                prePaidCard.Name = "Hdfc";
-            }
-            else
-            {
-                prePaidCard = new Icici();
-                prePaidCard.Name = "Icici";
-            }
-            return prePaidCard;
-        }
     }
-    class PostPaidCard
+    class Card
     {
         public string Name { get; set; }
-        public decimal AnnualFee { get; set; }
         public string Type { get; set; }
+    }
+    class PostPaidCard : Card
+    {
+        public decimal AnnualFee { get; set; }
         public string Balance { get; set; }
         public decimal Charge { get; set; }
     }
@@ -98,22 +78,5 @@ namespace DesignPattern
     class Smart : PostPaidCard
     {
 
-    }
-
-    //debit cards(prepaid)
-    class PrepaidCard
-    {
-        public string Name { get; set; }
-        public decimal AnnualFee { get; set; }
-        public string Type { get; set; }
-        public string Balance { get; set; }
-    }
-    class Icici : PrepaidCard
-    {
-
-    }
-    class Hdfc : PrepaidCard
-    {
-        public string Feedback { get; set; }
     }
 }
